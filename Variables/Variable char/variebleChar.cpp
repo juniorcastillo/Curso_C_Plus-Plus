dd3ff94893f0e1cc61ce1d880e03745759f7af18
Junior Miguel Castillo Santana <juniormiguelcastillosantana@gmail.com>
2017-07-04 20:35:40 +0200

    Variable char

---
+++ Variables/Variable char/variebleChar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+char* nombre= "Junior Castillo";
+
+int main(){
+	
+	cout << nombre << "\n \n" << endl;
+	system("PAUSE");
+	return 0;
+}

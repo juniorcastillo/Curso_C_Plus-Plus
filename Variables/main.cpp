f1b5a6de0a77215cfcd2f9f22f453fdc339c77ec
Junior Miguel Castillo Santana <juniormiguelcastillosantana@gmail.com>
2017-07-04 20:10:35 +0200

    Variables
    Suma de números enteros.

---
+++ Variables/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+int num=  10;
+int num2= 15;
+int suma= num + num2;
+using namespace std;
+
+int main(){
+	
+	cout << suma << "\n \n" << endl;
+	system("PAUSE");
+	return 0;
+	 
+} 

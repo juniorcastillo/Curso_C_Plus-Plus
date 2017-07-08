55c078e3ddd4986063cb9b86bf45a359ec5c6d2d
Junior Miguel Castillo Santana <juniormiguelcastillosantana@gmail.com>
2017-07-08 14:43:17 +0200

    Continuando...

---
+++ lectura de datos/letura_de_datos.cpp
@@ -0,0 +1,17 @@
+//Lectura o entrada de datos
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int numero;
+	
+	cout<<"Introduzca un numero: ";
+	cin>>numero;
+	
+	cout <<"El numero que ha introducido es: " << numero;
+	
+	return 0;
+}
